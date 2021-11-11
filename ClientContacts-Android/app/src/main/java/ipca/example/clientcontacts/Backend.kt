35cfb72fdb6e6e4dcbf4a990d163b180282d1955
Lourenço Gomes <leto.lg@gmail.com>
2021-11-11 08:48:23 +0000

    get

--- ClientContacts-Android/app/src/main/java/ipca/example/clientcontacts/Backend.kt
+++ ClientContacts-Android/app/src/main/java/ipca/example/clientcontacts/Backend.kt
@@ -6,14 +6,35 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import org.json.JSONArray
 import org.json.JSONObject
 
 object Backend {
 
-    const val BASE_API = "http://172.20.10.4:5001/"
+    const val BASE_API = "http://172.20.10.3:5001/"
 
-    fun fetchAllClients(){
+    fun fetchAllClients( callback: (List<Client>)->Unit ) {
+        GlobalScope.launch (Dispatchers.IO){
+            val client = OkHttpClient()
+            val request = Request.Builder()
+                .url(BASE_API+ "Client")
+                .build()
+            client.newCall(request).execute().use { response ->
+                if (response.isSuccessful){
+                    val result = response.body!!.string()
+                    val clientJsonArray = JSONArray(result)
+                    val clients = arrayListOf<Client>()
+                    for( index in 0 until clientJsonArray.length()){
+                        val client = Client.fromJson(clientJsonArray[index] as JSONObject)
+                        clients.add(client)
+                    }
 
+                    GlobalScope.launch (Dispatchers.Main){
+                        callback.invoke(clients)
+                    }
+                }
+            }
+        }
     }
 
     fun fetchClient(id:String, callback: (Client)->Unit ) {
--- ClientContacts-Android/app/src/main/java/ipca/example/clientcontacts/MainActivity.kt
+++ ClientContacts-Android/app/src/main/java/ipca/example/clientcontacts/MainActivity.kt
@@ -2,19 +2,50 @@ package ipca.example.clientcontacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ListView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    var clients = arrayListOf<Client>()
+    var adapter : ClientsAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        adapter = ClientsAdapter()
+        val listViewClients = findViewById<ListView>(R.id.listViewClients)
+        listViewClients.adapter = adapter
 
-        val textViewHello = findViewById<TextView>(R.id.textViewHello)
-
-        Backend.fetchClient("23r23", { textViewHello.text = it.name })
-
-        textViewHello.text = "A carregar ..."
+        Backend.fetchAllClients {
+            clients = it as ArrayList<Client>
+            adapter?.notifyDataSetChanged()
+        }
     }
 
+    inner class ClientsAdapter : BaseAdapter(){
+        override fun getCount(): Int {
+            return clients.size
+        }
+
+        override fun getItem(position: Int): Any {
+            return clients[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return 0
+        }
+
+        override fun getView(position: Int, parent: View?, viewGroup: ViewGroup?): View {
+            val rowView = layoutInflater.inflate(R.layout.row_client,viewGroup, false)
+            val textViewName = rowView.findViewById<TextView>(R.id.textViewName)
+            val textViewPhone = rowView.findViewById<TextView>(R.id.textViewPhone)
+            textViewName.text = clients[position].name
+            textViewPhone.text = clients[position].phone
+            return rowView
+        }
+
+    }
 }
